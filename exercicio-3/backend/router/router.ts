b811fd5a87eb82094b37bb7453c46c3935c8a802
Maycke <reydsonk@gmail.com>
2026-07-30 18:33:21 -0300

    fix: exercicio 3 finalizado, router terminado.

--- exercicio-3/backend/router/router.ts
+++ exercicio-3/backend/router/router.ts
@@ -3,6 +3,18 @@ import GitHubController from '../controller/controllerAdm'
 
 const router = Router();
 
-router.get('/api/index', GitHubController.getAll);
+/* Resumo de mudanças nas rotas
+                            async method()	        method = async () =>
+this no router direto	    ❌ perde	          ✅ preserva
+Precisa de função anônima	✅ sim	          ❌ não
+Estilo	                    Método tradicional  Arrow function */
+
+router.post('/api/users/:username', (req, res) => GitHubController.fetchAndSave(req, res));
+router.get('/api/users/:username/repos', (req, res) => GitHubController.repos(req, res));
+
+router.get('/api/users', (req, res) => GitHubController.getAll(req, res));
+
+router.get('/api/users/sum', (req, res) => GitHubController.sumRepos(req, res));
+router.get('/api/users/topfive', (req, res) => GitHubController.topFive(req, res));
 
 export default router;
